bafae218bea3b1005bb400eb14aac93fdf45a977
A.J.Rouvoet <arjen.rouvoet@stcorp.nl>
2016-08-05 16:40:09 +0200

    Remove collocation filtering from filter-phase in in-memory program execution
    As it cannot be treated as a simple dimension-filter.

--- libharp/harp-program-execute.c
+++ libharp/harp-program-execute.c
@@ -372,101 +372,6 @@ static int evaluate_valid_range_filters(const harp_product *product, harp_progra
     return 0;
 }
 
-static int evaluate_collocation_filter(const harp_product *product, harp_program *program,
-                                       harp_dimension_mask_set *dimension_mask_set)
-{
-    harp_variable *variable;
-    const harp_collocation_filter_args *args;
-    harp_collocation_result *collocation_result;
-    harp_predicate *predicate = NULL;
-    harp_dimension_type dimension_type[1] = { harp_dimension_time };
-    int use_collocation_index;
-    int i;
-
-    /* find the next collocation filter in the program */
-    for (i = 0; i < program->num_actions; i++)
-    {
-        if (program->action[i]->type == harp_action_filter_collocation)
-        {
-            break;
-        }
-    }
-
-    if (i == program->num_actions)
-    {
-        /* No collocation filter action present in action list. */
-        return 0;
-    }
-
-    /* Check for the presence of the 'collocation_index' or 'index' variable. Either variable should be 1-D and should
-     * depend on the time dimension.
-     */
-    if (find_variable(product, "collocation_index", 1, dimension_type, &variable) == 0)
-    {
-        use_collocation_index = 1;
-    }
-    else if (find_variable(product, "index", 1, dimension_type, &variable) == 0)
-    {
-        use_collocation_index = 0;
-    }
-    else
-    {
-        /* Neither the 'collocation_index' nor the 'index' variable exists in the product, which means the collocation
-         * (index) filter cannot be applied.
-         */
-        harp_set_error(HARP_ERROR_ACTION, "product does not contain a variable 'collocation_index' or 'index'");
-        return -1;
-    }
-
-    if (variable->data_type != harp_type_int32)
-    {
-        harp_set_error(HARP_ERROR_ACTION, "variable '%s' has data type '%s'; expected '%s'", variable->name,
-                       harp_get_data_type_name(variable->data_type), harp_get_data_type_name(harp_type_int32));
-        return -1;
-    }
-
-    /* Create filter predicate. */
-    args = (const harp_collocation_filter_args *)program->action[i]->args;
-    if (harp_collocation_result_read(args->filename, &collocation_result) != 0)
-    {
-        return -1;
-    }
-    if (harp_collocation_filter_predicate_new(collocation_result, product->source_product, args->filter_type,
-                                              use_collocation_index, &predicate) != 0)
-    {
-        harp_collocation_result_delete(collocation_result);
-        return -1;
-    }
-    else
-    {
-        harp_collocation_result_delete(collocation_result);
-    }
-
-    if (dimension_mask_set[harp_dimension_time] == NULL)
-    {
-        long dimension = product->dimension[harp_dimension_time];
-
-        /* Create dimension mask if necessary. */
-        if (harp_dimension_mask_new(1, &dimension, &dimension_mask_set[harp_dimension_time]) != 0)
-        {
-            harp_predicate_delete(predicate);
-            return -1;
-        }
-    }
-
-    if (harp_predicate_update_mask_all_1d(predicate, variable, dimension_mask_set[harp_dimension_time]) != 0)
-    {
-        harp_predicate_delete(predicate);
-        return -1;
-    }
-    else
-    {
-        harp_predicate_delete(predicate);
-    }
-
-    return 0;
-}
-
 static int evaluate_point_filters_0d(const harp_product *product, harp_program *program, uint8_t *product_mask)
 {
     harp_predicate_set *predicate_set;
@@ -894,10 +799,62 @@ static int evaluate_area_filters_1d(const harp_product *product, harp_program *p
     return 0;
 }
 
-/* execute 0..n variable filters (keep/exclude) from the head of program */
-static int execute_variable_filter_actions(harp_product *product, harp_program *program)
+/* execute the variable exclude filter from the head of program */
+static int execute_variable_exclude_filter_action(harp_product *product, harp_program *program)
+{
+    const harp_variable_exclusion_args *ex_args;
+    int variable_id;
+    int j;
+    harp_action *action;
+
+    assert(program->num_actions != 0);
+    action = program->action[0];
+    if (action->type != harp_action_exclude_variable)
+    {
+        return 0;
+    }
+
+    /* unmark the variables to exclude */
+    ex_args = (const harp_variable_exclusion_args *)action->args;
+    for (j = 0; j < ex_args->num_variables; j++)
+    {
+        if (harp_product_get_variable_id_by_name(product, ex_args->variable_name[j], &variable_id) != 0)
+        {
+            /* already removed, not an error */
+            continue;
+        }
+
+        /* execute the actin: remove the variable */
+        if (harp_product_remove_variable(product, product->variable[variable_id]) != 0)
+        {
+            return -1;
+        }
+    }
+
+    /* remove the action that we executed */
+    if (harp_program_remove_action_at_index(program, 0) != 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+/* execute the variable include filter from the head of program */
+static int execute_variable_include_filter_action(harp_product *product, harp_program *program)
 {
     uint8_t *include_variable_mask;
+    harp_action *action;
+    const harp_variable_inclusion_args *in_args;
+    int variable_id;
+    int j;
+
+    assert(program->num_actions != 0);
+    action = program->action[0];
+    if (action->type != harp_action_include_variable)
+    {
+        return 0;
+    }
 
     include_variable_mask = (uint8_t *)calloc(product->num_variables, sizeof(uint8_t));
     if (include_variable_mask == NULL)
@@ -907,83 +864,42 @@ static int execute_variable_filter_actions(harp_product *product, harp_program *
         return -1;
     }
 
-    /* Process all include and exclude actions that follow in one go,
-     * return on first different action */
-    while (program->num_actions > 0)
+    /* assume all variables are excluded */
+    for (j = 0; j < product->num_variables; j++)
     {
-        const harp_action *action;
-        const harp_variable_inclusion_args *in_args;
-        const harp_variable_exclusion_args *ex_args;
-        int variable_id;
-        int j;
+        include_variable_mask[j] = 0;
+    }
 
-        action = program->action[0];
-
-        /* continue while we process include/excludes */
-        if (action->type == harp_action_include_variable)
+    /* set the 'keep' flags in the mask */
+    in_args = (const harp_variable_inclusion_args *)action->args;
+    for (j = 0; j < in_args->num_variables; j++)
+    {
+        if (harp_product_get_variable_id_by_name(product, in_args->variable_name[j], &variable_id) != 0)
         {
-            /* assume all variables are excluded */
-            for (j = 0; j < product->num_variables; j++)
-            {
-                include_variable_mask[j] = 0;
-            }
-
-            /* set the 'keep' flags in the mask */
-            in_args = (const harp_variable_inclusion_args *)action->args;
-            for (j = 0; j < in_args->num_variables; j++)
-            {
-                if (harp_product_get_variable_id_by_name(product, in_args->variable_name[j], &variable_id) != 0)
-                {
-                    harp_set_error(HARP_ERROR_ACTION, "cannot keep non-existant variable '%s'",
-                                   in_args->variable_name[j]);
-                    goto error;
-                }
-
-                include_variable_mask[variable_id] = 1;
-            }
-
-            /* filter the variables using the mask */
-            for (j = product->num_variables - 1; j >= 0; j--)
-            {
-                if (!include_variable_mask[j])
-                {
-                    if (harp_product_remove_variable(product, product->variable[j]) != 0)
-                    {
-                        goto error;
-                    }
-                }
-            }
-        }
-        else if (action->type == harp_action_exclude_variable)
-        {
-            /* unmark the variables to exclude */
-            ex_args = (const harp_variable_exclusion_args *)action->args;
-            for (j = 0; j < ex_args->num_variables; j++)
-            {
-                if (harp_product_get_variable_id_by_name(product, ex_args->variable_name[j], &variable_id) != 0)
-                {
-                    /* already removed, not an error */
-                    continue;
-                }
-
-                /* execute the actin: remove the variable */
-                if (harp_product_remove_variable(product, product->variable[variable_id]) != 0)
-                {
-                    goto error;
-                }
-            }
-        }
-        else
-        {
-            /* stop processing actions from the program */
-            break;
-        }
-
-        /* remove the action that we execute */
-        if (harp_program_remove_action_at_index(program, 0) != 0)
-        {
+            harp_set_error(HARP_ERROR_ACTION, "cannot keep non-existant variable '%s'",
+                            in_args->variable_name[j]);
             goto error;
         }
+
+        include_variable_mask[variable_id] = 1;
+    }
+
+    /* filter the variables using the mask */
+    for (j = product->num_variables - 1; j >= 0; j--)
+    {
+        if (!include_variable_mask[j])
+        {
+            if (harp_product_remove_variable(product, product->variable[j]) != 0)
+            {
+                goto error;
+            }
+        }
+    }
+
+    /* remove the action that we execute */
+    if (harp_program_remove_action_at_index(program, 0) != 0)
+    {
+        goto error;
     }
 
     free(include_variable_mask);
@@ -994,53 +910,47 @@ static int execute_variable_filter_actions(harp_product *product, harp_program *
     return -1;
 }
 
-/* Execute the collocation filter action at the head of program */
+/* run a collocation filter action at the head of program */
 static int execute_collocation_filter(harp_product *product, harp_program *program)
 {
-    int i;
+    harp_action *action;
+    const harp_collocation_filter_args *args;
+    harp_collocation_mask *collocation_mask;
 
-    i = 0;
-    while (i < program->num_actions)
+    assert(program->num_actions != 0);
+    action = program->action[0];
+    if (action->type != harp_action_filter_collocation)
     {
-        harp_action *action;
-        const harp_collocation_filter_args *args;
-        harp_collocation_mask *collocation_mask;
+        /* Action is not a collocation filter action, skip it. */
+        return 0;
+    }
 
-        action = program->action[i];
-        if (action->type != harp_action_filter_collocation)
-        {
-            /* Action is not a collocation filter action, skip it. */
-            i++;
-            continue;
-        }
+    if (product->source_product == NULL)
+    {
+        harp_set_error(HARP_ERROR_INVALID_ARGUMENT, "product attribute 'source_product' is NULL");
+        return -1;
+    }
 
-        if (product->source_product == NULL)
-        {
-            harp_set_error(HARP_ERROR_INVALID_ARGUMENT, "product attribute 'source_product' is NULL");
-            return -1;
-        }
+    args = (const harp_collocation_filter_args *)action->args;
+    if (harp_collocation_mask_import(args->filename, args->filter_type, product->source_product,
+                                        &collocation_mask) != 0)
+    {
+        return -1;
+    }
 
-        args = (const harp_collocation_filter_args *)action->args;
-        if (harp_collocation_mask_import(args->filename, args->filter_type, product->source_product,
-                                         &collocation_mask) != 0)
-        {
-            return -1;
-        }
+    if (harp_product_apply_collocation_mask(collocation_mask, product) != 0)
+    {
+        harp_collocation_mask_delete(collocation_mask);
+        return -1;
+    }
+    else
+    {
+        harp_collocation_mask_delete(collocation_mask);
+    }
 
-        if (harp_product_apply_collocation_mask(collocation_mask, product) != 0)
-        {
-            harp_collocation_mask_delete(collocation_mask);
-            return -1;
-        }
-        else
-        {
-            harp_collocation_mask_delete(collocation_mask);
-        }
-
-        if (harp_program_remove_action_at_index(program, i) != 0)
-        {
-            return -1;
-        }
+    if (harp_program_remove_action_at_index(program, 0) != 0)
+    {
+        return -1;
     }
 
     return 0;
@@ -1050,16 +960,24 @@ static int action_is_dimension_filter(const harp_action *action)
 {
     switch (action->type)
     {
-        case harp_action_exclude_variable:
-        case harp_action_include_variable:
-        case harp_action_derive_variable:
-            return 0;
-        default:
+        case harp_action_filter_comparison:
+        case harp_action_filter_string_comparison:
+        case harp_action_filter_bit_mask:
+        case harp_action_filter_membership:
+        case harp_action_filter_string_membership:
+        case harp_action_filter_valid_range:
+        case harp_action_filter_longitude_range:
+        case harp_action_filter_point_distance:
+        case harp_action_filter_area_mask_covers_point:
+        case harp_action_filter_area_mask_covers_area:
+        case harp_action_filter_area_mask_intersects_area:
             return 1;
+        default:
+            return 0;
     }
 }
 
-/* execute the prefix of the passed program of 0..n dimension filter actions */
+/* execute the prefix of the program of 0..n dimension filter actions */
 static int execute_filter_actions(harp_product *product, harp_program *program)
 {
     uint8_t product_mask = 1;
@@ -1112,6 +1030,7 @@ static int execute_filter_actions(harp_product *product, harp_program *program)
     {
         return -1;
     }
+    /* TODO Shouldn't point filters just raise errors when longitude/latitude is missing? */
     if (find_variable(product, "longitude", 0, NULL, NULL) == 0
         && find_variable(product, "latitude", 0, NULL, NULL) == 0)
     {
@@ -1138,11 +1057,6 @@ static int execute_filter_actions(harp_product *product, harp_program *program)
     /* Second filter pass (1-D variables). */
     if (harp_dimension_mask_set_new(&dimension_mask_set) != 0)
     {
-        return -1;
-    }
-    if (evaluate_collocation_filter(product, dimension_filters, dimension_mask_set) != 0)
-    {
-        harp_dimension_mask_set_delete(dimension_mask_set);
         return -1;
     }
     if (evaluate_value_filters_1d(product, dimension_filters, dimension_mask_set) != 0)
@@ -1230,12 +1144,6 @@ static int execute_filter_actions(harp_product *product, harp_program *program)
         return 0;
     }
 
-    /* Collocation filter. */
-    if (execute_collocation_filter(product, dimension_filters) != 0)
-    {
-        return -1;
-    }
-
     /* Verify that all dimension filters have been executed */
     if (dimension_filters->num_actions != 0)
     {
@@ -1290,8 +1198,14 @@ static int execute_next_action(harp_product *product, harp_program *program)
     switch (action->type)
     {
         case harp_action_exclude_variable:
+            if (execute_variable_exclude_filter_action(product, program))
+            {
+                return -1;
+            }
+
+            break;
         case harp_action_include_variable:
-            if (execute_variable_filter_actions(product, program))
+            if (execute_variable_include_filter_action(product, program))
             {
                 return -1;
             }
@@ -1300,6 +1214,14 @@ static int execute_next_action(harp_product *product, harp_program *program)
 
         case harp_action_derive_variable:
             if (execute_derivation(product, program) != 0)
+            {
+                return -1;
+            }
+
+            break;
+
+        case harp_action_filter_collocation:
+            if (execute_collocation_filter(product, program) != 0)
             {
                 return -1;
             }
