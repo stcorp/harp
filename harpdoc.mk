1a3187be6d96187bb5f6ba1951ad0de1222b9cdf
Sander Niemeijer <niemeijer@stcorp.nl>
2017-01-19 13:23:11 +0100

    Update generated docs

--- harpdoc.mk
+++ harpdoc.mk
@@ -242,6 +242,7 @@ HARP_DOCFILES = \
 	doc/html/ingestions/GOSAT_FTS_L2_CH4_TC.html \
 	doc/html/ingestions/GOSAT_FTS_L2_CO2_TC.html \
 	doc/html/ingestions/HIRDLS_L2.html \
+	doc/html/ingestions/IASI_L1.html \
 	doc/html/ingestions/IASI_L2.html \
 	doc/html/ingestions/MIPAS_NL_L2.html \
 	doc/html/ingestions/MLS_L2_BRO.html \
@@ -840,6 +841,8 @@ doc/html/ingestions/GOSAT_FTS_L2_CH4_TC.html:
 doc/html/ingestions/GOSAT_FTS_L2_CO2_TC.html:
 	$(MAKE) harp_doc
 doc/html/ingestions/HIRDLS_L2.html:
+	$(MAKE) harp_doc
+doc/html/ingestions/IASI_L1.html:
 	$(MAKE) harp_doc
 doc/html/ingestions/IASI_L2.html:
 	$(MAKE) harp_doc
