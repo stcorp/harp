60b591ff6db4e7fdaa4682761b22dd002efc8679
Sander Niemeijer <niemeijer@stcorp.nl>
2017-01-25 23:01:40 +0100

    Update documentation

--- harpdoc.mk
+++ harpdoc.mk
@@ -226,6 +226,7 @@ HARP_DOCFILES = \
 	doc/html/ingestions/GEOMS-TE-UVVIS-DOAS-ZENITH-GAS-007-SO2.html \
 	doc/html/ingestions/GOME2_L2_O3MNTO.html \
 	doc/html/ingestions/GOME2_L2_O3MOTO.html \
+	doc/html/ingestions/GOME_L1_EXTRACTED.html \
 	doc/html/ingestions/GOME_L2.html \
 	doc/html/ingestions/GOME_L2_ERSNTO.html \
 	doc/html/ingestions/GOME_L2_ERSOTO.html \
@@ -809,6 +810,8 @@ doc/html/ingestions/GEOMS-TE-UVVIS-DOAS-ZENITH-GAS-007-SO2.html:
 doc/html/ingestions/GOME2_L2_O3MNTO.html:
 	$(MAKE) harp_doc
 doc/html/ingestions/GOME2_L2_O3MOTO.html:
+	$(MAKE) harp_doc
+doc/html/ingestions/GOME_L1_EXTRACTED.html:
 	$(MAKE) harp_doc
 doc/html/ingestions/GOME_L2.html:
 	$(MAKE) harp_doc
