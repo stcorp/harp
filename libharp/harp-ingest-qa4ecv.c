a20cf36f7832897d0f92961fe2914e645151e80b
Sander Niemeijer <niemeijer@stcorp.nl>
2017-03-25 14:33:47 +0100

    Add tropospheric_NO2_column_number_density_avk

--- libharp/harp-ingest-qa4ecv.c
+++ libharp/harp-ingest-qa4ecv.c
@@ -695,6 +695,44 @@ static int read_surface_albedo_hcho(void *user_data, harp_array data)
                         info->num_scanlines * info->num_pixels, data);
 }
 
+static int read_no2_column(void *user_data, harp_array data)
+{
+    ingest_info *info = (ingest_info *)user_data;
+    char *variable_name;
+
+    variable_name = info->use_summed_total_column ? "summed_no2_total_vertical_column" : "total_no2_vertical_column";
+    return read_dataset(info->detailed_results_cursor, variable_name, harp_type_float,
+                        info->num_scanlines * info->num_pixels, data);
+}
+
+static int read_no2_column_uncertainty(void *user_data, harp_array data)
+{
+    ingest_info *info = (ingest_info *)user_data;
+    char *variable_name;
+
+    variable_name = info->use_summed_total_column ? "summed_no2_total_vertical_column_uncertainty" :
+    "total_no2_vertical_column_uncertainty";
+
+    return read_dataset(info->detailed_results_cursor, variable_name, harp_type_float,
+                        info->num_scanlines * info->num_pixels, data);
+}
+
+static int read_no2_column_amf(void *user_data, harp_array data)
+{
+    ingest_info *info = (ingest_info *)user_data;
+
+    return read_dataset(info->product_cursor, "amf_total", harp_type_float, info->num_scanlines * info->num_pixels,
+                        data);
+}
+
+static int read_no2_column_avk(void *user_data, harp_array data)
+{
+    ingest_info *info = (ingest_info *)user_data;
+
+    return read_dataset(info->product_cursor, "averaging_kernel", harp_type_float,
+                        info->num_scanlines * info->num_pixels * info->num_layers, data);
+}
+
 static int read_no2_column_tropospheric(void *user_data, harp_array data)
 {
     ingest_info *info = (ingest_info *)user_data;
@@ -727,42 +765,85 @@ static int read_no2_column_tropospheric_validity(void *user_data, harp_array dat
                         info->num_scanlines * info->num_pixels, data);
 }
 
-static int read_no2_column(void *user_data, harp_array data)
+static int read_no2_column_tropospheric_avk(void *user_data, harp_array data)
 {
     ingest_info *info = (ingest_info *)user_data;
-    char *variable_name;
+    harp_array layer_data;
+    harp_array amf_data;
+    long i, j;
 
-    variable_name = info->use_summed_total_column ? "summed_no2_total_vertical_column" : "total_no2_vertical_column";
-    return read_dataset(info->detailed_results_cursor, variable_name, harp_type_float,
-                        info->num_scanlines * info->num_pixels, data);
-}
+    if (read_dataset(info->product_cursor, "averaging_kernel", harp_type_float,
+                     info->num_scanlines * info->num_pixels * info->num_layers, data) != 0)
+    {
+        return -1;
+    }
 
-static int read_no2_column_uncertainty(void *user_data, harp_array data)
-{
-    ingest_info *info = (ingest_info *)user_data;
-    char *variable_name;
+    layer_data.int32_data = malloc(info->num_scanlines * info->num_pixels * sizeof(int32_t));
+    if (layer_data.int32_data == NULL)
+    {
+        harp_set_error(HARP_ERROR_OUT_OF_MEMORY, "out of memory (could not allocate %lu bytes) (%s:%u)",
+                       info->num_scanlines * info->num_pixels * sizeof(int32_t), __FILE__, __LINE__);
+        return -1;
+    }
+    if (read_dataset(info->product_cursor, "tm5_tropopause_layer_index", harp_type_int32,
+                     info->num_scanlines * info->num_pixels, layer_data) != 0)
+    {
+        free(layer_data.int32_data);
+        return -1;
+    }
 
-    variable_name = info->use_summed_total_column ? "summed_no2_total_vertical_column_uncertainty" :
-        "total_no2_vertical_column_uncertainty";
+    amf_data.float_data = malloc(info->num_scanlines * info->num_pixels * sizeof(float));
+    if (amf_data.float_data == NULL)
+    {
+        harp_set_error(HARP_ERROR_OUT_OF_MEMORY, "out of memory (could not allocate %lu bytes) (%s:%u)",
+                       info->num_scanlines * info->num_pixels * sizeof(float), __FILE__, __LINE__);
+        free(layer_data.int32_data);
+        return -1;
+    }
+    if (read_no2_column_amf(user_data, amf_data) != 0)
+    {
+        free(amf_data.float_data);
+        free(layer_data.int32_data);
+        return -1;
+    }
+    for (i = 0; i < info->num_scanlines * info->num_pixels; i++)
+    {
+        if (layer_data.int32_data[i] < 0 || layer_data.int32_data[i] >= info->num_layers)
+        {
+            for (j = 0; j < info->num_layers; j++)
+            {
+                data.float_data[i * info->num_layers + j] = harp_nan();
+            }
+        }
+        else
+        {
+            for (j = 0; j < layer_data.int32_data[i]; j++)
+            {
+                data.float_data[i * info->num_layers + j] *= amf_data.float_data[i];
+            }
+            for (j = layer_data.int32_data[i]; j < info->num_layers; j++)
+            {
+                data.float_data[i * info->num_layers + j] = 0;
+            }
+        }
+    }
+    free(layer_data.int32_data);
 
-    return read_dataset(info->detailed_results_cursor, variable_name, harp_type_float,
-                        info->num_scanlines * info->num_pixels, data);
-}
+    if (read_no2_column_tropospheric_amf(user_data, amf_data) != 0)
+    {
+        free(amf_data.float_data);
+        return -1;
+    }
+    for (i = 0; i < info->num_scanlines * info->num_pixels; i++)
+    {
+        for (j = 0; j < info->num_layers; j++)
+        {
+            data.float_data[i * info->num_layers + j] /= amf_data.float_data[i];
+        }
+    }
+    free(amf_data.float_data);
 
-static int read_no2_column_amf(void *user_data, harp_array data)
-{
-    ingest_info *info = (ingest_info *)user_data;
-
-    return read_dataset(info->product_cursor, "amf_total", harp_type_float, info->num_scanlines * info->num_pixels,
-                        data);
-}
-
-static int read_no2_column_avk(void *user_data, harp_array data)
-{
-    ingest_info *info = (ingest_info *)user_data;
-
-    return read_dataset(info->product_cursor, "averaging_kernel", harp_type_float,
-                        info->num_scanlines * info->num_pixels * info->num_layers, data);
+    return 0;
 }
 
 static int read_hcho_column_tropospheric(void *user_data, harp_array data)
@@ -1095,6 +1176,17 @@ static void register_no2_product(void)
                                                    NULL, read_no2_column_tropospheric_validity);
     path = "/PRODUCT/SUPPORT_DATA/DETAILED_RESULTS/processing_quality_flags[]";
     harp_variable_definition_add_mapping(variable_definition, NULL, NULL, path, NULL);
+
+    description = "averaging kernel for the tropospheric vertical column number density of NO2";
+    variable_definition =
+        harp_ingestion_register_variable_full_read(product_definition, "tropospheric_NO2_column_number_density_avk",
+                                                   harp_type_float, 2, dimension_type, NULL, description,
+                                                   HARP_UNIT_DIMENSIONLESS, NULL, read_no2_column_tropospheric_avk);
+    path = "/PRODUCT/averaging_kernel[], /PRODUCT/amf_total[], /PRODUCT/amf_trop[], "
+        "/PRODUCT/tm5_tropopause_layer_index[]";
+    description = "averaging_kernel[layer] = if layer <= tm5_tropopause_layer_index then "
+        "averaging_kernel[layer] * amf_total / amf_trop else 0";
+    harp_variable_definition_add_mapping(variable_definition, NULL, NULL, path, description);
 
     description = "tropospheric air mass factor, computed by integrating the altitude dependent air mass factor over "
         "the atmospheric layers from the surface up to and including the layer with the tropopause";
