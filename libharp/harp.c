stcorp/harp

commit 7d17d0726b684c13ac719a96a7f8a7dc8cb34a54

Fix memory leak

libharp/harp.c
@@ -549,10 +549,6 @@ LIBHARP_API int harp_import(const char *filename, const char *operations, const 
         }
 
         /* try ingest */
-        if (harp_product_new(&imported_product) != 0)
-        {
-            return -1;
-        }
         if (harp_ingest(filename, operations, options, &imported_product) != 0)
         {
             harp_product_delete(imported_product);

commit 75778ad21b8e925a08bd6a699cdc2f5d30a6eacc

Fix invalid free()
Regression from 7d17d0726b684c13ac719a96a7f8a7dc8cb34a54

libharp/harp.c
@@ -551,7 +551,6 @@ LIBHARP_API int harp_import(const char *filename, const char *operations, const 
         /* try ingest */
         if (harp_ingest(filename, operations, options, &imported_product) != 0)
         {
-            harp_product_delete(imported_product);
             return -1;
         }
     }