fb8bf395517078e022c91512f70b8d9b194a998e
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2021-10-22 15:11:54 +0200

    Indent

--- libharp/harp-ingest-omi_l2.c
+++ libharp/harp-ingest-omi_l2.c
@@ -2408,8 +2408,7 @@ static void register_spacecraft_altitude_variable(harp_product_definition *produ
     description = "altitude of Aura spacecraft";
     variable_definition = harp_ingestion_register_variable_full_read(product_definition, "sensor_altitude",
                                                                      harp_type_double, 1, dimension_type, NULL,
-                                                                     description, "m", NULL,
-                                                                     read_spacecraft_altitude);
+                                                                     description, "m", NULL, read_spacecraft_altitude);
     harp_variable_definition_add_mapping(variable_definition, NULL, NULL, path, NULL);
 }
 
