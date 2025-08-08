e1d04f52c11394b21f34c2dd9b3b439df24d431a
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-08-08 15:59:32 +0200

    Add optimization for finding derivations based on chemical species
    The majority of derivations are for variables linked to a specific chemical species.
    And the majority of those can only be performed if there is already a variable for that species in the product.
    This change introduces a mechanism where, if we are looking for source variables for a derivation, we directly mark the source variable as impossible if there was no variable in the product for the same species and the variable could only have been derived that way.
    
    This especially solves the problem with the recently added derivation for (dry) air column number density from species column number density and species (dry air) volume mixing ratio, and where trying to look for a derivation for the air number density had become very slow.
    
    With this change the maximum search depth is changed back from 8 to 10 again.

--- libharp/harp-derived-variable.c
+++ libharp/harp-derived-variable.c
@@ -47,6 +47,7 @@ typedef struct conversion_info_struct
     int num_dimensions;
     harp_dimension_type dimension_type[HARP_MAX_NUM_DIMS];
     uint8_t *skip;      /* 1: variable cannot be created; 2: variable cannot be used because of cyclic dependency */
+    int8_t product_contains_species[harp_num_chemical_species]; /* -1: not set, 0: no, 1: maybe */
     int depth;
     int max_depth;
     harp_variable *variable;
@@ -94,6 +95,24 @@ static int has_dimension_types(const harp_variable *variable, int num_dimensions
     }
 
     return 1;
+}
+
+/* 0: definitely not, 1: maybe */
+static int product_has_species(const harp_product *product, harp_chemical_species species)
+{
+    const char *species_name = harp_chemical_species_name(species);
+    int i;
+
+    for (i = 0; i < product->num_variables; i++)
+    {
+        if (strstr(product->variable[i]->name, species_name) != NULL)
+        {
+            /* may not be an exact match, but mark as possible */
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
 static char *get_dimsvar_name(const char *variable_name, int num_dimensions, const harp_dimension_type *dimension_type)
@@ -158,7 +177,7 @@ static int conversion_info_init(conversion_info *info, const harp_product *produ
     info->num_dimensions = 0;
     info->skip = NULL;
     info->depth = 0;
-    info->max_depth = 8;
+    info->max_depth = 10;
     info->variable = NULL;
 
     info->skip = malloc(harp_derived_variable_conversions->num_variables);
@@ -169,6 +188,7 @@ static int conversion_info_init(conversion_info *info, const harp_product *produ
         return -1;
     }
     memset(info->skip, 0, harp_derived_variable_conversions->num_variables);
+    memset(info->product_contains_species, -1, harp_num_chemical_species);
 
     return 0;
 }
@@ -459,6 +479,24 @@ static int find_source_variables(conversion_info *info, harp_source_variable_def
     }
 
     conversion_list = harp_derived_variable_conversions->conversions_for_variable[index];
+
+    if (conversion_list->specific_species != harp_chemical_species_unknown)
+    {
+        if (info->product_contains_species[conversion_list->specific_species] == -1)
+        {
+            /* initialize */
+            info->product_contains_species[conversion_list->specific_species] =
+                product_has_species(info->product, conversion_list->specific_species);
+        }
+        if (info->product_contains_species[conversion_list->specific_species] == 0)
+        {
+            /* permanently mark this variable as something that cannot be derived */
+            info->skip[index] = 1;
+
+            /* conversion not possible since no variables for the same species exist in the product */
+            return 1;
+        }
+    }
 
     for (i = 0; i < conversion_list->num_conversions; i++)
     {
@@ -901,10 +939,11 @@ void harp_variable_conversion_delete(harp_variable_conversion *conversion)
 }
 
 /* this function also adds the conversion to the global derived variable conversion list */
-int harp_variable_conversion_new(const char *variable_name, harp_data_type data_type, const char *unit,
-                                 int num_dimensions, harp_dimension_type *dimension_type,
-                                 long independent_dimension_length, harp_conversion_function set_variable_data,
-                                 harp_variable_conversion **new_conversion)
+int harp_variable_conversion_species_new(const char *variable_name, harp_data_type data_type, const char *unit,
+                                         int num_dimensions, harp_dimension_type *dimension_type,
+                                         long independent_dimension_length, harp_chemical_species specific_species,
+                                         harp_conversion_function set_variable_data,
+                                         harp_variable_conversion **new_conversion)
 {
     harp_variable_conversion *conversion = NULL;
     int i;
@@ -931,6 +970,7 @@ int harp_variable_conversion_new(const char *variable_name, harp_data_type data_
     conversion->source_description = NULL;
     conversion->set_variable_data = set_variable_data;
     conversion->enabled = NULL;
+    conversion->specific_species = specific_species;
 
     conversion->dimsvar_name = get_dimsvar_name(variable_name, num_dimensions, dimension_type);
     if (conversion->dimsvar_name == NULL)
@@ -961,6 +1001,16 @@ int harp_variable_conversion_new(const char *variable_name, harp_data_type data_
     *new_conversion = conversion;
 
     return 0;
+}
+
+int harp_variable_conversion_new(const char *variable_name, harp_data_type data_type, const char *unit,
+                                 int num_dimensions, harp_dimension_type *dimension_type,
+                                 long independent_dimension_length, harp_conversion_function set_variable_data,
+                                 harp_variable_conversion **new_conversion)
+{
+    return harp_variable_conversion_species_new(variable_name, data_type, unit, num_dimensions, dimension_type,
+                                                independent_dimension_length, harp_chemical_species_unknown,
+                                                set_variable_data, new_conversion);
 }
 
 int harp_variable_conversion_add_source(harp_variable_conversion *conversion, const char *variable_name,
